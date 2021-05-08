1c7bdd364117bb1c3b7c3beab34bcc0f74b0c9c5
ArtemPaT <60203224+ArtemPaT@users.noreply.github.com>
2021-05-08 09:43:21 +0900

    Add files via upload

--- mpitfin/index.php
+++ mpitfin/index.php
@@ -37,9 +37,12 @@
 		.br-15 {
 			border-radius: 15px;
 		}
+		.bg-bg {
+			background: #222222;
+		}
 	</style>
 </head>
-<body style="font-family: Cera">
+<body style="font-family: Cera; overflow-x: hidden;">
 	<nav class="navbar fixed p-3 pl-4 bg-black text-white d-flex">
 		<h3><span class="strange-color">In</span>SPIRIT</h3>
 		<div class="col-3 d-flex">
@@ -70,6 +73,57 @@
 		</div>
 		
 	</div>
+	<h1 class="text-center">ЧТО МЫ?</h1>
+	<div class="bg-bg text-white" style="height: 2000px;">
+		<div class="row">
+			<div class="col-6 pl-5" style="margin-top: 200px;">
+				<h1>МЫ ЭТО ЛЮБИМАЯ РАБОТА</h1>
+				<h5>Найдите своего клиента и зарабатывайте на любимом деле в удобных условиях :)</h5>
+			</div>
+			<div class="col-6">
+				<img src="sss.PNG" class="w-100">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-6">
+				<img src="saa.PNG" class="w-100">
+			</div>
+			<div class="col-6 pl-5" style="margin-top: 200px;">
+				<h1>МЫ ЭТО НОВЫЕ ЗНАКОМСТВА</h1>
+				<h5>Находите с кем создать новые интересные коллабы и просто хорошего друга в общей сфере</h5>
+			</div>
+			
+		</div>
+		<div class="row">
+			<div class="col-6 pl-5" style="margin-top: 200px;">
+				<h1>МЫ ЭТО ВДОХНОВЕНИЕ</h1>
+				<h5>Черпайте новые знания и концепты, создавайте на их основе свой полностью новый и уникальный продукт!</h5>
+			</div>
+			<div class="col-6">
+				<img src="asa.PNG" class="w-100">
+			</div>
+		</div>
+		<img src="Снимок.PNG" class="w-100">
+		
+		
+	</div>
+	<div class="bg-bg col-12 text-white" style="position: absolute; top: 3500px; padding: 50px;">
+		<div class="row">
+			<div class="col-4">
+				<h3>О нас</h3>
+				<h3>Контакты</h3>
+			</div>
+			<div class="col-4">
+				<h3>Условия пользования</h3>
+				<h3>Политика конфиденциальности</h3>
+			</div>
+			<div class="col-4">
+				<h1 style="position: absolute; right: 10px"><span class="strange-color">In</span>SPIRIT</h1>
+			</div>
+		</div>
+		
+	</div>
+
 
 </body>
 </html>
